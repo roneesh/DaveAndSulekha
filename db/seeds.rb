44fd3892035c30c764b2f150fda66e8241251397
Roneesh Vashisht <roneesh@gmail.com>
2013-06-02 18:04:13 -0500

    image source updating working and in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,28 +12,30 @@ webster_desc = "These fabrics are from around town in Hoshiarpur, when the heat 
 img_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYHIutQvJpBupYxSh2QUDebWrpPGkceaoZIwxmi1vIIRW9P8pK7w"
 
 products = [
-  {name: "The R.P. Sharma in Green", price: 21, description: rp_desc },
-  {name: "The R.P. Sharma in Blue", price: 21, description: rp_desc },
-  {name: "The R.P. Sharma in Juniper and Yellow", price: 21, description: rp_desc },
-  {name: "The R.P. Sharma in Navy and Red", price: 21, description: rp_desc },
-  {name: "The D.N. Vashisht in Turqoise", price: 14, description: dn_desc },
-  {name: "The D.N. Vashisht in Metal", price: 14, description: dn_desc },
-  {name: "The D.N. Vashisht in Blue", price: 14, description: dn_desc },
-  {name: "The D.N. Vashisht in Pink", price: 14, description: dn_desc },
-  {name: "The Webster in Red", price: 8, description: webster_desc },
-  {name: "The Webster in Orange", price: 8, description: webster_desc },
-  {name: "The Webster in Gingham", price: 8, description: webster_desc }
+  {name: "The R.P. Sharma in Green", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152200914/the-rp-sharma-pocket-square-in-green" },
+  {name: "The R.P. Sharma in Blue", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152204599/the-rp-sharma-pocket-square-in-blue" },
+  {name: "The R.P. Sharma in Juniper and Yellow", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152202304/the-rp-sharma-pocket-square-in-juniper"},
+  {name: "The R.P. Sharma in Navy and Red", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152201830/the-rp-sharma-pocket-square-in-navy-and"},
+  {name: "The D.N. Vashisht in Turqoise Pastel", price: 14, description: dn_desc, etsy_link: "https://www.etsy.com/listing/152754916/the-dn-vashisht-pocket-square-in"},
+  {name: "The D.N. Vashisht in Metal", price: 14, description: dn_desc, etsy_link: "https://www.etsy.com/listing/152757211/the-dn-vashisht-pocket-square-in-metal"},
+  {name: "The D.N. Vashisht in Blue Pastel", price: 14, description: dn_desc, etsy_link: "https://www.etsy.com/listing/152754320/the-dn-vashisht-pocket-square-in-blue"},
+  {name: "The D.N. Vashisht in Pink Pastel", price: 14, description: dn_desc, etsy_link: "https://www.etsy.com/listing/152753598/the-dn-vashisht-pocket-square-in-pink"},
+  {name: "The Webster in Red", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152759797/the-webster-pocket-square-in-red"},
+  {name: "The Webster in Orange", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152759637/the-webster-pocket-square-in-orange" },
+  {name: "The Webster in Gingham", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152755192/the-webster-pocket-square-in-gingham"}
 ]
 
 products.each do |product|
   p = Product.create product
-  3.times do |n|
+  4.times do |n|
     if n == 0
       ProductImage.create(product_id: p.id, image_type: "flat", source: img_url )
     elsif n == 1
       ProductImage.create(product_id: p.id, image_type: "navy_suit", source: img_url)
     elsif n == 2
       ProductImage.create(product_id: p.id, image_type: "stone_suit", source: img_url)
+    elsif n == 3
+      ProductImage.create(product_id: p.id, image_type: "gallery", source: img_url)
     end
   end
 end
