3f662f195be928dc787a36ba8cf725258eef0619
Roneesh Vashisht <roneesh@gmail.com>
2013-06-04 21:46:18 -0500

    updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ webster_desc = "These fabrics are from around town in Hoshiarpur, when the heat 
 img_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYHIutQvJpBupYxSh2QUDebWrpPGkceaoZIwxmi1vIIRW9P8pK7w"
 
 products = [
-  {name: "The R.P. Sharma in Green", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152200914/the-rp-sharma-pocket-square-in-green" },
+  {name: "The R.P. Sharma in Green", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152200914/the-rp-sharma-pocket-square-in-green", name_partial: "rp-green" },
   {name: "The R.P. Sharma in Blue", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152204599/the-rp-sharma-pocket-square-in-blue" },
   {name: "The R.P. Sharma in Juniper and Yellow", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152202304/the-rp-sharma-pocket-square-in-juniper"},
   {name: "The R.P. Sharma in Navy and Red", price: 21, description: rp_desc, etsy_link: "https://www.etsy.com/listing/152201830/the-rp-sharma-pocket-square-in-navy-and"},
@@ -33,20 +33,20 @@ products = [
 ]
 
 products.each do |product|
-  p = Product.create product
+  p = Product.create(name: product[:name], price: product[:price], description: product[:description], etsy_link: product[:etsy_link])
   puts "Created product #{p.id}..."
   4.times do |n|
     if n == 0
-      ProductImage.create(product_id: p.id, image_type: "flat", source: img_url )
+      ProductImage.create(product_id: p.id, image_type: "flat", source: "https://s3.amazonaws.com/dave-and-sulekha-images/im-#{product[:name_partial]}-flat.JPG" )
       puts "Created product image for product #{p.id}"
     elsif n == 1
-      ProductImage.create(product_id: p.id, image_type: "navy_suit", source: img_url)
+      ProductImage.create(product_id: p.id, image_type: "navy_suit", source: "https://s3.amazonaws.com/dave-and-sulekha-images/im-#{product[:name_partial]}-navycoat.JPG")
       puts "Created product image for product #{p.id}"
     elsif n == 2
-      ProductImage.create(product_id: p.id, image_type: "stone_suit", source: img_url)
+      ProductImage.create(product_id: p.id, image_type: "stone_suit", source: "https://s3.amazonaws.com/dave-and-sulekha-images/im-#{product[:name_partial]}-stonecoat.JPG")
       puts "Created product image for product #{p.id}"
     elsif n == 3
-      ProductImage.create(product_id: p.id, image_type: "gallery", source: img_url)
+      ProductImage.create(product_id: p.id, image_type: "gallery", source: "https://s3.JPG")
       puts "Created product image for product #{p.id}"
     end
   end
