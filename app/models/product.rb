e16b06c7d76cb2568c78bf3fdfacbf089e8ab1f6
Roneesh Vashisht <roneesh@gmail.com>
2013-06-04 20:38:49 -0500

    ajax working locally with new model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,16 @@ class Product < ActiveRecord::Base
 
   has_many :product_images
 
+  def flat_image_src
+    self.product_images.where(image_type: "flat").sample.source
+  end
+
+  def navy_suit_image_src
+    self.product_images.where(image_type: "navy_suit").sample.source
+  end
+
+  def stone_suit_image_src  
+    self.product_images.where(image_type: "stone_suit").sample.source
+  end
 
 end
--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -3,4 +3,6 @@ class ProductImage < ActiveRecord::Base
 
   belongs_to :product
 
+
+
 end
