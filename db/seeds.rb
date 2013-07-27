d497793900b34b56e8b2fd570234dbfd2b3950dd
Roneesh Vashisht <roneesh@gmail.com>
2013-07-27 17:36:18 -0400

    updating front-page styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ puts "Product Images destroyd!"
 
 rp_desc = "The R.P. Sharma is a line of plaids I sourced in New Delhi. Running around the city I looked for bleeding madras, we ended up finding these few bolts in the basement of a fabric shop in an electronics market. When I saw them, my breath was taken away. Just like the old madras, alive with color, these are the square for summer. "
 dn_desc = "I found these pastels in Chandigargh while sourcing fabric in the city of Corbusier. 'What's the provenance of these fabrics?' I asked. 'Hand loomed, from the villages sir'. These are rough hewn squares, made by our family tailor in Hoshiarpur, fold 'em, stuff 'em, they're the casual square. They still smell like India too."
-# webster_desc = "These fabrics are from around town in Hoshiarpur, when the heat abated I would go shopping, these were rescued from the corner of a fabric shop located in a meat market. These are really rough squares, basically unfinished, as they unfinish more with wear and washing they'll be nice."
+webster_desc = "These fabrics are from around town in Hoshiarpur, when the heat abated I would go shopping, these were rescued from the corner of a fabric shop located in a meat market. These are really rough squares, basically unfinished, as they unfinish more with wear and washing they'll be nice."
 img_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYHIutQvJpBupYxSh2QUDebWrpPGkceaoZIwxmi1vIIRW9P8pK7w"
 
 products = [
@@ -27,13 +27,13 @@ products = [
   {name: "The D.N. in Metal", product_line: "The Perfect Pastel", price: 14, description: dn_desc, etsy_link: "https://www.etsy.com/listing/152757211/the-dn-vashisht-pocket-square-in-metal", name_partial: "dn-metal"},
   {name: "The D.N. in Blue", product_line: "The Perfect Pastel", price: 14, description: dn_desc, etsy_link: "https://www.etsy.com/listing/152754320/the-dn-vashisht-pocket-square-in-blue", name_partial: "dn-blue"},
   {name: "The D.N. in Pink", product_line: "The Perfect Pastel", price: 14, description: dn_desc, etsy_link: "https://www.etsy.com/listing/152753598/the-dn-vashisht-pocket-square-in-pink", name_partial: "dn-pink"},
-  # {name: "The Webster in Red", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152759797/the-webster-pocket-square-in-red", name_partial: "webster-red"},
-  # {name: "The Webster in Orange", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152759637/the-webster-pocket-square-in-orange", name_partial: "webster-orange" },
-  # {name: "The Webster in Gingham", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152755192/the-webster-pocket-square-in-gingham", name_partial: "webster-gingham"}
+  {name: "The Webster in Red", product_line: "Perfect Plaids and Gingham", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152759797/the-webster-pocket-square-in-red", name_partial: "webster-red"},
+  {name: "The Webster in Orange", product_line: "Perfect Plaids and Gingham", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152759637/the-webster-pocket-square-in-orange", name_partial: "webster-orange" },
+  {name: "The Webster in Gingham", product_line: "Perfect Plaids and Gingham", price: 8, description: webster_desc, etsy_link: "https://www.etsy.com/listing/152755192/the-webster-pocket-square-in-gingham", name_partial: "webster-gingham"}
 ]
 
 products.each do |product|
-  p = Product.create(name: product[:name], price: product[:price], description: product[:description], etsy_link: product[:etsy_link])
+  p = Product.create(name: product[:name], product_line: product[:product_line],   price: product[:price], description: product[:description], etsy_link: product[:etsy_link])
   puts "Created product #{p.id}..."
   5.times do |n|
     if n == 0
